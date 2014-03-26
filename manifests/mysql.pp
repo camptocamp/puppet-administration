3f451b25defe9f0bb5fd1a53531510e8974c3fcd
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-03-26 12:08:37 +0100

    Create mysql-admin group

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -2,12 +2,20 @@ class administration::mysql (
   $sudo_user = $sudo_mysql_admin_user,
 ) {
 
-  $sudo_group = '%mysql-admin'
-  $sudo_user_alias = flatten([$sudo_group, $sudo_user])
+  $sudo_user_alias = flatten(['%mysql-admin', $sudo_user])
   $sudo_cmnd = $::osfamily ? {
     Debian => '/etc/init.d/mysql',
     RedHat => '/etc/init.d/mysqld, /sbin/service mysqld',
   }
+
+  ensure_resource(
+    'group',
+    'mysql-admin',
+    {
+      ensure => present,
+      system => true,
+    }
+  )
 
   sudo::conf { 'mysql-administration':
     ensure  => present,
