62ccee8406ca3b51b1b4713062178adcba56d61e
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-04-09 11:15:15 +0200

    Add administration::tomcat

---
+++ manifests/tomcat.pp
@@ -0,0 +1,32 @@
+# == Class: administration::tomcat
+#
+# Creates a "tomcat-admin" group and use sudo to allows members of this group
+# to:
+# - su to the tomcat user (which allows to kill java, remove lockfiles, etc)
+# - restart tomcat instances.
+#
+# Requires:
+# - definition sudo::conf from module saz/puppet-sudo
+#
+# Warning: will overwrite /etc/sudoers !
+#
+class administration::tomcat (
+  $sudo_user = $sudo_tomcat_admin_user,
+) {
+
+  $sudo_group = '%tomcat-admin'
+  $sudo_user_alias = flatten([$sudo_group, $sudo_user])
+  $sudo_cmnd = '/etc/init.d/tomcat-*, /bin/su tomcat, /bin/su - tomcat'
+
+  group { 'tomcat-admin':
+    ensure => present,
+    system => true,
+  }
+
+  sudo::conf { 'tomcat-administration':
+    ensure  => present,
+    content => template('administration/tomcat/sudoers.erb'),
+    require => Group['tomcat-admin'],
+  }
+
+}
