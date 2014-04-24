dfab8aa5ba7d2e593507a2914d9ed0bfe1d283b6
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-04-24 14:52:31 +0200

    Add administration::postgresql

---
+++ manifests/postgresql.pp
@@ -0,0 +1,29 @@
+# == Class: administration::postgresql
+#
+# This class will create a "postgresql-admin" group and add a couple of rules
+# to /etc/sudoers allowing members of this group to administer postgresql
+# databases.
+#
+# Requires:
+#  - definition sudo::conf from module saz/puppet-sudo
+#
+class administration::postgresql (
+  $sudo_user = '',
+) {
+  $sudo_group = '%postgresql-admin'
+  $sudo_user_alias = flatten([$sudo_group, $sudo_user])
+  $sudo_cmnd = '/etc/init.d/postgresql, /etc/init.d/postgresql-*, /bin/su postgres, /bin/su - postgres'
+
+  group { 'postgresql-admin':
+    ensure => present,
+    system => true,
+  }
+
+  sudo::conf { 'postgresql-administration':
+    ensure  => present,
+    content => template('administration/postgresql/sudoers.erb'),
+    require => Group['postgresql-admin'],
+  }
+
+}
+
