943b6476331bad8c220c41509e8240d43366be13
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-04-30 15:29:32 +0200

    Add administration::apache

---
+++ manifests/apache.pp
@@ -0,0 +1,38 @@
+# == Class administration::apache
+#
+class administration::apache (
+  $sudo_user = undef,
+) {
+
+  $distro_specific_apache_sudo = $::osfamily ? {
+    Debian => '/usr/sbin/apache2ctl',
+    RedHat => '/usr/sbin/apachectl, /sbin/service httpd',
+  }
+
+  group { 'apache-admin':
+    ensure => present,
+    system => true,
+  }
+
+  # used in erb template
+  $wwwpkgname = $::osfamily ? {
+    Debian => 'apache2',
+    RedHat => 'httpd',
+  }
+
+  $wwwuser    = $::osfamily ? {
+    Debian => 'www-data',
+    RedHat => 'apache',
+  }
+
+  $sudo_group = '%apache-admin'
+  $sudo_user_alias = flatten([$sudo_group, $sudo_user])
+  $sudo_cmnd = "/etc/init.d/${wwwpkgname}, /bin/su ${wwwuser}, /bin/su - ${wwwuser}, ${distro_specific_apache_sudo}"
+
+  sudo::conf { 'apache-administration':
+    ensure  => present,
+    content => template('administration/apache/sudoers.erb'),
+    require => Group['apache-admin'],
+  }
+
+}
