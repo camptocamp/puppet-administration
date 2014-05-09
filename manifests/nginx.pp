3450c9f5d98a2fb7003aa633a9021aa7b3e8cf0f
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-05-09 14:44:28 +0200

    Add administration::nginx

---
+++ manifests/nginx.pp
@@ -0,0 +1,22 @@
+class administration::nginx(
+  $sudo_user = undef,
+) {
+  $nginx_user = $::operatingsystem ? {
+    Debian => 'www-data',
+    Ubuntu => 'www-data',
+    RedHat => 'nginx',
+    Fedora => 'nginx',
+    CentOS => 'nginx',
+  }
+
+  group { 'nginx-admin':
+    ensure => present,
+    system => true,
+  }
+  ->
+  sudo::conf { 'nginx-administration':
+    ensure  => present,
+    content => template('administration/nginx/sudoers.erb'),
+  }
+
+}
