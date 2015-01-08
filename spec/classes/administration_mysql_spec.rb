9ca20a2ed4f0aa40a928deb4765de9b244bd722c
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-01-08 13:43:18 +0100

    Fix unit tests

--- spec/classes/administration_mysql_spec.rb
+++ spec/classes/administration_mysql_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
 
 describe 'administration::mysql' do
-  let(:facts) { {
-    :lsbdistcodename => 'wheezy',
-    :operatingsystem => 'Debian',
-    :osfamily        => 'Debian',
-  } }
-  it { should compile.with_all_deps }
+
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts.merge({
+          :puppetversion => Puppet::PUPPETVERSION,
+          :sudoversion   => '1.8.0',
+        })
+      end
+
+      it { should compile.with_all_deps }
+    end
+  end
 end
