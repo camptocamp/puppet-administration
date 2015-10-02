0cf1409966094b503fc060a35590bc45d7f58b72
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2015-10-02 11:08:20 +0200

    new adminstration for varnish service and reload

---
+++ manifests/varnish.pp
@@ -0,0 +1,14 @@
+class administration::varnish(
+  $sudo_user = undef,
+) {
+  group { 'varnish-admin':
+    ensure => present,
+    system => true,
+  }
+  ->
+  sudo::conf { 'varnish-administration':
+    ensure  => present,
+    content => template('administration/varnish/sudoers.erb'),
+  }
+
+}
