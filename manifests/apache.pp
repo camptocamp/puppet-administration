3c053dcc5b86844dfe7d0b0348bf52905eb5722f
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2018-01-09 16:39:14 +0100

    Make apachectl command configurable

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -4,16 +4,15 @@ class administration::apache (
   $sudo_user = undef,
   $service_user = undef,
   $service_name = undef,
+  $additional_commands = undef,
 ) {
 
-  $distro_specific_apache_sudo = $::osfamily ? {
-    'Debian' => '/usr/sbin/apache2ctl',
-    'RedHat' => '/usr/sbin/apachectl, /sbin/service httpd',
-  }
-
-  group { 'apache-admin':
-    ensure => present,
-    system => true,
+  $_additional_commands = $additional_commands ? {
+    undef => $::osfamily ? {
+      'Debian' => '/usr/sbin/apache2ctl',
+      'RedHat' => '/usr/sbin/apachectl, /sbin/service httpd',
+    },
+    default => $additional_commands,
   }
 
   $_service_name = $service_name ? {
@@ -32,9 +31,14 @@ class administration::apache (
     default => $service_user,
   }
 
+  group { 'apache-admin':
+    ensure => present,
+    system => true,
+  }
+
   $sudo_group = '%apache-admin'
   $sudo_user_alias = flatten([$sudo_group, $sudo_user])
-  $sudo_cmnd = "/etc/init.d/${_service_name}, /usr/bin/systemctl * ${_service_name}, /bin/systemctl * ${_service_name}, /bin/su ${_service_user}, /bin/su - ${_service_user}, ${distro_specific_apache_sudo}"
+  $sudo_cmnd = "/etc/init.d/${_service_name}, /usr/bin/systemctl * ${_service_name}, /bin/systemctl * ${_service_name}, /bin/su ${_service_user}, /bin/su - ${_service_user}, ${_additional_commands}"
 
   sudo::conf { 'apache-administration':
     ensure  => present,
